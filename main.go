2b7c4ed676ea6bcc1820ffe6deebd25d7d074291
lordvorath <4kill3@gmail.com>
2025-07-11 14:21:42 +0000

    channel refactor

--- main.go
+++ main.go
@@ -18,28 +18,44 @@ func main() {
 	}
 	defer f.Close()
 
+	incData := getLinesChannel(f)
+	for s := range incData {
+		fmt.Printf("read: %s\n", s)
+	}
+
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
 	var buffer = make([]byte, 8)
 	var str string
-	for {
-		n, err := f.Read(buffer)
-		if err != nil {
-			if str != "" {
-				fmt.Printf("read: %s\n", str)
-				str = ""
-			}
-			if errors.Is(err, io.EOF) {
+	var outData = make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(outData)
+		for {
+			n, err := f.Read(buffer)
+			if err != nil {
+				if str != "" {
+					outData <- str
+					str = ""
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
 				break
 			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
-		}
 
-		parts := strings.Split(string(buffer[:n]), "\n")
+			parts := strings.Split(string(buffer[:n]), "\n")
 
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s%s\n", str, parts[i])
-			str = ""
+			for i := 0; i < len(parts)-1; i++ {
+				str += parts[i]
+				outData <- str
+				str = ""
+			}
+			str += parts[len(parts)-1]
 		}
-		str += parts[len(parts)-1]
-	}
+	}()
+
+	return outData
 }
