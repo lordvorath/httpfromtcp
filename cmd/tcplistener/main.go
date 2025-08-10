16ed59f6e5385dda9ecad9ed1459ffde150b841c
lordvorath <4kill3@gmail.com>
2025-08-10 09:12:56 +0200

    connect parser to tcplistener

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
+
+	"github.com/lordvorath/httpfromtcp/internal/request"
 )
 
 func main() {
@@ -24,44 +23,11 @@ func main() {
 
 		fmt.Println("Connection established from:", netConn.RemoteAddr())
 
-		incData := getLinesChannel(netConn)
-		for s := range incData {
-			fmt.Println(s)
-		}
-		fmt.Printf("\nThe channel has been closed\n")
+		req, err := request.RequestFromReader(netConn)
+		fmt.Println("Request line:")
+		fmt.Printf("- Method: %s\n", req.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", req.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", req.RequestLine.HttpVersion)
+
 	}
-}
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	var buffer = make([]byte, 8)
-	var str string
-	var outData = make(chan string)
-	go func() {
-		defer f.Close()
-		defer close(outData)
-		for {
-			n, err := f.Read(buffer)
-			if err != nil {
-				if str != "" {
-					outData <- str
-				}
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				fmt.Printf("error: %s\n", err.Error())
-				break
-			}
-
-			parts := strings.Split(string(buffer[:n]), "\n")
-
-			for i := 0; i < len(parts)-1; i++ {
-				str += parts[i]
-				outData <- str
-				str = ""
-			}
-			str += parts[len(parts)-1]
-		}
-	}()
-
-	return outData
 }
